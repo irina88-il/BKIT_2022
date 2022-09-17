8d6bdb958be56e2f9efec9bede6bc8131e50e3e8
Irina.L <100842377+irina88-il@users.noreply.github.com>
2022-09-17 22:40:18 +0300

    Update qr.py

--- code/lab1_code/qr.py
+++ code/lab1_code/qr.py
@@ -4,52 +4,69 @@ import math
 def get_coef(index, prompt):
     '''
     Читаем коэффициент из командной строки или вводим с клавиатуры
-
     Args:
         index (int): Номер параметра в командной строке
         prompt (str): Приглашение для ввода коэффицента
-
     Returns:
         float: Коэффициент квадратного уравнения
     '''
+
     try:
         # Пробуем прочитать коэффициент из командной строки
         coef_str = sys.argv[index]
     except:
-        # Вводим с клавиатуры
-        print(prompt)
-        coef_str = input()
-    # Переводим строку в действительное число
-    coef = float(coef_str)
+        while True:
+            try:
+                # Вводим с клавиатуры
+                print(prompt)
+                coef_str = input()
+                # Переводим строку в действительное число
+                coef = float(coef_str)
+                break
+            except ValueError:
+                print("Вы ввели не число. Попробуйте снова. ")
     return coef
 
 
 def get_roots(a, b, c):
     '''
-    Вычисление корней квадратного уравнения
-
+    Вычисление корней ,биквадратного уравнения
     Args:
         a (float): коэффициент А
         b (float): коэффициент B
         c (float): коэффициент C
-
     Returns:
         list[float]: Список корней
     '''
-    result = []
-    D = b*b - 4*a*c
-    if D == 0.0:
-        root = -b / (2.0*a)
-        result.append(root)
-    elif D > 0.0:
-        sqD = math.sqrt(D)
-        root1 = (-b + sqD) / (2.0*a)
-        root2 = (-b - sqD) / (2.0*a)
-        result.append(root1)
-        result.append(root2)
+    try:
+        result = []
+        D = b*b - 4*a*c
+        if D == 0.0:
+            y = -b / (2.0*a)
+            if y >= 0:
+                root_1 = math.sqrt(y)
+                root_2 = -math.sqrt(y)
+                result.append(root_1)
+                result.append(root_2)      
+        elif D > 0.0:
+            sqD = math.sqrt(D)
+            y1 = (-b + sqD) / (2.0*a)
+            y2 = (-b - sqD) / (2.0*a)
+            if (y1 >= 0):
+                root1_1 = math.sqrt(y1)
+                root1_2 = -math.sqrt(y1)
+                result.append(root1_1)
+                result.append(root1_2)
+            if (y2 >= 0):
+                root2_1 = math.sqrt(y2)
+                root2_2 = -math.sqrt(y2)
+                result.append(root2_1)
+                result.append(root2_2)
+                
+    except ZeroDivisionError:
+            print("Коэффициент А не может быть равен нулю.")
     return result
-
-
+    
 def main():
     '''
     Основная функция
@@ -61,12 +78,12 @@ def main():
     roots = get_roots(a,b,c)
     # Вывод корней
     len_roots = len(roots)
-    if len_roots == 0:
+    if len_roots == 0 and a != 0:
         print('Нет корней')
-    elif len_roots == 1:
-        print('Один корень: {}'.format(roots[0]))
     elif len_roots == 2:
-        print('Два корня: {} и {}'.format(roots[0], roots[1]))
+        print('Два корня: {:.2f} и {:.2f}'.format(roots[0], roots[1]))
+    elif len_roots == 4:
+        print('Четыре корня: {:.2f}, {:.2f}, {:.2f} и {:.2f}'.format(roots[0], roots[1], roots[2], roots[3]))
     
 
 # Если сценарий запущен из командной строки
@@ -75,3 +92,4 @@ if __name__ == "__main__":
 
 # Пример запуска
 # qr.py 1 0 -4
+
